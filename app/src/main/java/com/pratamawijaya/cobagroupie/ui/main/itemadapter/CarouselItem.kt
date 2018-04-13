50e8378df2b72e15028b7f81f1199242b5753541
pratamawijaya <set.mnemonix@gmail.com>
2018-04-13 19:47:46 +0700

    add snaphelper

--- app/src/main/java/com/pratamawijaya/cobagroupie/ui/main/itemadapter/CarouselItem.kt
+++ app/src/main/java/com/pratamawijaya/cobagroupie/ui/main/itemadapter/CarouselItem.kt
@@ -1,6 +1,7 @@
 package com.pratamawijaya.cobagroupie.ui.main.itemadapter
 
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.LinearSnapHelper
 import android.support.v7.widget.RecyclerView
 import com.pratamawijaya.cobagroupie.R
 import com.xwray.groupie.GroupAdapter
@@ -11,7 +12,8 @@ import kotlinx.android.synthetic.main.layout_item_carousel.view.rvCarousel
 /**
  * Created by pratama on 13/04/18.
  */
-class CarouselItem(val carouselDecoration: RecyclerView.ItemDecoration, val carouselAdapter: GroupAdapter<ViewHolder>
+class CarouselItem(private val carouselDecoration: RecyclerView.ItemDecoration,
+                   private val carouselAdapter: GroupAdapter<ViewHolder>
 ) : Item() {
 
     override fun bind(viewHolder: com.xwray.groupie.kotlinandroidextensions.ViewHolder, position: Int) {
@@ -22,6 +24,8 @@ class CarouselItem(val carouselDecoration: RecyclerView.ItemDecoration, val caro
             removeItemDecoration(carouselDecoration)
             addItemDecoration(carouselDecoration)
         }
+        val linearSnapHelper = LinearSnapHelper()
+        linearSnapHelper.attachToRecyclerView(viewHolder.itemView.rvCarousel)
     }
 
     override fun getLayout(): Int = R.layout.layout_item_carousel
